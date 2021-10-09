ab854f4638a5fc7e73d8866709ca5d658528f7a8
RedBeanN <me@hongshn.xyz>
2021-10-09 10:13:33 +0800

    use polling signal

--- src/utils/Signal.js
+++ src/utils/Signal.js
@@ -1,24 +1,25 @@
-// const waitFor = require('./waitFor');
+const waitFor = require('./waitFor');
 
 class Signal {
   constructor () {
-    this.signalList = {
-      'authed': [],
-      'verified': [],
-    };
+    // this.signalList = {
+    //   'authed': [],
+    //   'verified': [],
+    // };
+    this.signalList = [];
   }
   on (signalName, callback) {
-    if (!this.signalList[signalName]) this.signalList[signalName] = [];
-    this.signalList[signalName].push(callback);
-    // waitFor(() => {
-    //   if (this.signalList.includes(signalName)) return true;
-    //   return false;
-    // }, callback);
+    // if (!this.signalList[signalName]) this.signalList[signalName] = [];
+    // this.signalList[signalName].push(callback);
+    waitFor(() => {
+      if (this.signalList.includes(signalName)) return true;
+      return false;
+    }, callback);
   }
   trigger (signalName) {
-    // this.signalList.push(signalName);
-    if (!this.signalList[signalName]) return;
-    for (let callback of this.signalList[signalName]) callback();
+    this.signalList.push(signalName);
+    // if (!this.signalList[signalName]) return;
+    // for (let callback of this.signalList[signalName]) callback();
   }
 }
 
